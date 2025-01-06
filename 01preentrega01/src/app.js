02d6e424e1be6ee5e8be1bc52deb764cd1f2d12f
luisliquin <luisliquin@outlook.com.ar>
2025-01-05 22:27:20 -0300

    coreccion .env

---
+++ 01preentrega01/src/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
+import adoptionsRouter from './routes/adoption.router.js';
+import sessionsRouter from './routes/sessions.router.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT||8080;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.set('strictQuery', true);
+
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conexión exitosa a MongoDB'))
+  .catch(err => console.error('Error de conexión a MongoDB:', err));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users',usersRouter);
+app.use('/api/pets',petsRouter);
+app.use('/api/adoptions',adoptionsRouter);
+app.use('/api/sessions',sessionsRouter);
+
+app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
